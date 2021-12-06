6b034d5ee6b39d8acb1cd9a4c3e2b023afd487b9
ECR_EXCALIBUR_G770 <ECR_EXCALIBUR_G770@DESKTOP-TUMHS1A>
2021-12-06 16:17:56 +0300

    Add project files.

---
+++ MCD_OOPCokBicimlilikOrnek/Banka.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_OOPCokBicimlilikOrnek
+{
+    class Banka
+    {
+        public string BankaAdi { get; set; }
+        
+        private decimal _alisveristutari=0;
+
+        public decimal AlisverisTutari { get; set; }
+
+        public void BilgileriAl()
+        {
+            Console.WriteLine(BankaAdi + " bankasına hoşgeldiniz...");
+            Console.WriteLine("Alışveriş Tutarını giriniz: ");
+
+            _alisveristutari = Convert.ToDecimal(Console.ReadLine());
+            if (_alisveristutari>0)
+            {
+                AlisverisTutari = _alisveristutari;
+            }
+            else
+            {
+                throw new Exception("Tutar Sıfırdan büyük olmalıdır.");
+            }
+        }
+
+        public virtual void IndirimOraninaGoreIndirim()
+        {
+            AlisverisTutari -= AlisverisTutari * 0.20m;
+            Console.WriteLine("%20 indirim uygulandı...");
+            Console.WriteLine("Ödenecek tutar" + AlisverisTutari);
+        }
+  
+        
+
+    }
+}
---
+++ MCD_OOPCokBicimlilikOrnek/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCD_OOPCokBicimlilikOrnek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Polimorphism 
+
+            Console.WriteLine("Banka seçiniz A/B : ");
+            ConsoleKeyInfo secim;
+            secim = Console.ReadKey();
+            
+            if (secim.Key==ConsoleKey.A)
+            {
+                ABankasi A1 = new ABankasi();
+                A1.BilgileriAl();
+                A1.IndirimOraninaGoreIndirim();
+            }
+            else if (secim.Key == ConsoleKey.B)
+            {
+                BBankasi B1 = new BBankasi();
+                B1.BilgileriAl();
+                B1.IndirimOraninaGoreIndirim();
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
